49ff051f280f418860fe3aeddff3aebd1bd23039
agbarama <agbaramafavour4@gmail.com>
2024-03-07 12:09:36 +0100

    Created and styled ResetGame component.

---
+++ src/components/ResetGame.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import styles from "../styles/resetgame.module.css";
+const ResetGame = ({
+  setWinner,
+  setIsWinner,
+  setMovesWin,
+  setPlayMessage,
+  moves,
+  setMoves,
+}) => {
+  const handleReset = () => {
+    setWinner("");
+    setIsWinner(false);
+    setMovesWin(false);
+    setPlayMessage("Start game or select player");
+
+    // map and set all objects containing a move to an empty string and win to false
+    moves.map((move) => {
+      if (move.move !== "") {
+        move.win = false;
+        move.move = "";
+      }
+    });
+    setMoves(moves);
+  };
+
+  return (
+    <div className={styles.move}>
+      <div className={styles.resetGame} onClick={handleReset}>
+        Reset Game
+      </div>
+      <div>Taunt</div>
+    </div>
+  );
+};
+
+export default ResetGame;
